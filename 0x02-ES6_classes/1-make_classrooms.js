2ddaf783e256d4e09da220de0d6aee014416684e
DaffiX <suleydaffy@gmail.com>
2023-07-26 18:47:29 +0300

    updated sln

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -8,14 +8,4 @@ function initializeRooms() {
   ];
 }
 
-export default initializeRooms;import Classroom from './0-classroom';
-
-function initializeRooms() {
-  return [
-    new Classroom(19),
-    new Classroom(20),
-    new Classroom(34),
-  ];
-}
-
 export default initializeRooms;
